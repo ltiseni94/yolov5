211a482bb23feb5f1c891f4848aee55f8a7d5ef7
ltiseni94 <luca13tiseni@gmail.com>
2022-06-20 17:07:07 +0200

    record_camera has now a option for just opening the camera without recording

--- record_camera.py
+++ record_camera.py
@@ -8,15 +8,17 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('source', type=int, help="VideoCapture source")
     parser.add_argument('--output', '-o', default=None, help="Output video path")
+    parser.add_argument('--no-record', '-n', action='store_true', default=False)
     ns = parser.parse_args()
 
     source = ns.source
     output = ns.output
+    record = not ns.no_record
 
     if output is None:
         videos = [video for video in os.listdir('grasp_dataset')
                   if video.endswith('.mp4') and video.startswith('exp_')]
-        ids = [int(video.lstrip('exp_').rstrip('.mp4')) for video in videos]
+        ids = [int(video.lstrip('exp_')[:-4]) for video in videos]
         if len(ids) > 0:
             new_id = max(ids) + 1
         else:
@@ -27,9 +29,11 @@ def main():
             output += '.mp4'
 
     cap = cv2.VideoCapture(source)
-    width, height = cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    writer = cv2.VideoWriter(output, cv2.VideoWriter_fourcc(*'mp4v'), int(fps), (int(width), int(height)))
+    writer = None
+    if record:
+        width, height = cap.get(cv2.CAP_PROP_FRAME_WIDTH), cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        writer = cv2.VideoWriter(output, cv2.VideoWriter_fourcc(*'mp4v'), int(fps), (int(width), int(height)))
     if not cap.isOpened():
         raise IOError(f'Could not open videocapture at source {source}')
     try:
@@ -38,7 +42,8 @@ def main():
             if not res:
                 raise IOError(f'Could not read frame')
             cv2.imshow('camera', frame)
-            writer.write(frame)
+            if writer is not None:
+                writer.write(frame)
             k = cv2.waitKey(1) & 0xff
             if k in (ord('q'), ord('\x1b')):
                 break
@@ -47,7 +52,8 @@ def main():
     finally:
         cv2.destroyAllWindows()
         cap.release()
-        writer.release()
+        if writer is not None:
+            writer.release()
 
 
 if __name__ == '__main__':
