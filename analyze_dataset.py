352206cc84d127c695ea7a15abdbbebd8a293d33
ltiseni94 <luca13tiseni@gmail.com>
2022-06-20 17:05:05 +0200

    analyze dataset has now a filter for already existent predicted labels files

--- analyze_dataset.py
+++ analyze_dataset.py
@@ -2,13 +2,20 @@ import os
 import subprocess
 
 if __name__ == '__main__':
+    labels = ['grasp_dataset/' + label for label in os.listdir('grasp_dataset')
+              if label.startswith('exp_') and label.endswith('_true.csv')]
+
     videos = ['grasp_dataset/' + video for video in os.listdir('grasp_dataset')
               if video.startswith('exp_') and video.endswith('.mp4')]
+
+    for label in labels:
+        try:
+            videos.remove(f'{label.rstrip("_true.csv")}.mp4')
+            print(f'Already analyzed: {label.rstrip("_true.csv")}.mp4')
+        except ValueError:
+            pass
+
     for video in videos:
         subprocess.run(f'python app.py '
-                       f'--weights yolov5s.pt '
                        f'--source {video} '
-                       f'--iou-thres 0.2 '
-                       f'--conf-thres 0.35 '
-                       f'--agnostic-nms '
                        f'--save-label-file', shell=True)
